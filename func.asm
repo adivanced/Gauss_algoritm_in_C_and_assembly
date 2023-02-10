0ef45aa20a774aa333bc145fd1c9657145da8450
Ivan Panin <46397843+adivanced@users.noreply.github.com>
2023-02-10 19:11:33 +0300

    Update func.asm

--- func.asm
+++ func.asm
@@ -567,56 +567,52 @@ determinant_fpu: ; rdi - det ptr, rsi - ptr to equ copy, rdx - width, rcx - heig
 			pop rdi
 			mov r9, 1
 			fld dword [rsi] ; st0 - a11
+			lea rax, [rsi+rdx*4]
 			._mul_diag_loop:
-				mov rax, r8
-				mul r9
-				lea rax, [rsi+rax*4]
 				fld dword [rax+r9*4] ; st0 - aii, st1 - a11
 				fmulp st1, st0 
 				inc r9
+				lea rax, [rax+rdx*4]
 				cmp r9, rcx
 			jnz ._mul_diag_loop
 		dw 0x3fdb  ; NASM is kinda dumb. Should be the same as fstp oword [rdi]
 	xor rax, rax
 ret
 
-._diag_next_zero_straight: ; r10 - row number with zero
-	lea r12, [r10+1]
-
+._diag_next_zero_straight:
+	lea r10, [r9+2]
+	cmp r10, rcx
+	jae ._fixed_kinda
 	._diag_next_zero_straight_loop:
-		mov rax, r8
-		xor rdx, rdx
-		mul r12
-		lea rax, [rsi+rax*4]
+		lea r8, [r11+rdx*4]
 
 		pxor xmm4, xmm4
-		movss xmm0, [rax+r10*4]
+		movss xmm0, [r8+r9*4+4]
 		cmpss xmm4, xmm0, 0
-		movd edx, xmm4
-		and edx, edx 
+		movd edi, xmm4
+		and edi, edi
 		jz ._diag_next_zero_straight_loop_add
-		inc r12
-		cmp r12, rcx
+		inc r10
+		lea r8, [r8+rdx*4]
+		cmp r10, rcx
 	jnz ._diag_next_zero_straight_loop
 	jmp ._fixed_kinda
 
 	._diag_next_zero_straight_loop_add:
-		mov rdx, r8
-
-		shl rdx, 2
-		add rax, rdx
-		add r11, rdx
-		shr rdx, 5
+		mov rdi, rdx
+		lea r8, [r8+rdx*4]
+		lea r11, [r11+rdx*4]
+		shr rdi, 3
 
 		._diag_next_zero_straight_loop_add_sm_loop:
-			sub rax, 32
+			sub r8, 32
 			sub r11, 32
 			vmovups ymm0, [r11]
-			vmovups ymm1, [rax]
+			vmovups ymm1, [r8]
 			vaddps ymm0, ymm0, ymm1
 			vmovups [r11], ymm0
-			dec rdx
-		jnz ._diag_next_zero_straight_loop_add
+			dec rdi
+		jnz ._diag_next_zero_straight_loop_add_sm_loop
 	jmp ._fixed_kinda
 
 ; align 8
@@ -779,7 +775,5 @@ ret
 ; .end3:
 ; mov rax, 22
 ; ret
-
-
 
 
